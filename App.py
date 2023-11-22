c664111b8fc23d534b0d8c810129aba701d6198d
Khushman1303 <151705438+Khushman1303@users.noreply.github.com>
2023-11-22 10:30:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,66 +3,67 @@ import streamlit as st
 from tkinter import *
 from tkinter import messagebox
 import base64
-import os
 
 def decrypt():
-    password=code.get()
+    password = code.get()
 
-    if password=="1234":
-        screen2=Toplevel(screen)
-        screen2.title("decryption")
-        screen2.geometry("400x200")
-        screen2.configure(bg="#00bd56")
+    try:
+        if password == "1234":
+            screen2 = Toplevel(screen)
+            screen2.title("decryption")
+            screen2.geometry("400x200")
+            screen2.configure(bg="#00bd56")
 
-        message=text1.get(1.0,END)
-        decode_message=message.encode("ascii")
-        base64_bytes=base64.b64decode(decode_message)
-        decrypt=base64_bytes.decode("ascii")
+            message = text1.get(1.0, END)
+            decode_message = message.encode("ascii")
+            base64_bytes = base64.b64decode(decode_message)
+            decrypt = base64_bytes.decode("ascii")
 
+            Label(screen2, text="DECRYPT", font="arial", fg="white", bg="#00bd56").place(x=10, y=0)
+            text2 = Text(screen2, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+            text2.place(x=10, y=40, width=380, height=150)
 
-        Label(screen2,text="DECRYPT",font="arial",fg="white",bg="#00bd56").place(x=10,y=0)
-        text2=Text(screen2,font="Rpbote 10", bg="white",relief=GROOVE,wrap=WORD,bd=0)
-        text2.place(x=10,y=40,width=380,height=150)
+            text2.insert(END, decrypt)
 
-        text2.insert(END,decrypt)
+        elif password == "":
+            messagebox.showerror("Decryption", "Input Password")
 
-    elif password=="":
-        messagebox.showerror("encryption","Input Password")
+        elif password != "1234":
+            messagebox.showerror("Decryption", "Invalid Password")
 
-    elif password !="1234":
-        messagebox.showerror("encryption","Invalid Password")
-        
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred during decryption: {e}")
 
 
 def encrypt():
-    password=code.get()
+    password = code.get()
 
-    if password=="1234":
-        screen1=Toplevel(screen)
-        screen1.title("encryption")
-        screen1.geometry("400x200")
-        screen1.configure(bg="#ed3833")
+    try:
+        if password == "1234":
+            screen1 = Toplevel(screen)
+            screen1.title("encryption")
+            screen1.geometry("400x200")
+            screen1.configure(bg="#ed3833")
 
-        message=text1.get(1.0,END)
-        encode_message=message.encode("ascii")
-        base64_bytes=base64.b64encode(encode_message)
-        encrypt=base64_bytes.decode("ascii")
+            message = text1.get(1.0, END)
+            encode_message = message.encode("ascii")
+            base64_bytes = base64.b64encode(encode_message)
+            encrypt = base64_bytes.decode("ascii")
 
+            Label(screen1, text="ENCRYPT", font="arial", fg="white", bg="#ed3833").place(x=10, y=0)
+            text2 = Text(screen1, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
+            text2.place(x=10, y=40, width=380, height=150)
 
-        Label(screen1,text="ENCRYPT",font="arial",fg="white",bg="#ed3833").place(x=10,y=0)
-        text2=Text(screen1,font="Rpbote 10", bg="white",relief=GROOVE,wrap=WORD,bd=0)
-        text2.place(x=10,y=40,width=380,height=150)
+            text2.insert(END, encrypt)
 
-        text2.insert(END,encrypt)
+        elif password == "":
+            messagebox.showerror("Encryption", "Input Password")
 
-    elif password=="":
-        messagebox.showerror("encryption","Input Password")
+        elif password != "1234":
+            messagebox.showerror("Encryption", "Invalid Password")
 
-    elif password !="1234":
-        messagebox.showerror("encryption","Invalid Password")
-        
-        
-        
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred during encryption: {e}")
 
 def main_screen():
 
