a13162e56a533cca62ba114251dfcc4c6b627517
Khushman1303 <151705438+Khushman1303@users.noreply.github.com>
2023-11-22 11:03:50 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,109 +1,37 @@
 import streamlit as st
-
-from tkinter import *
-from tkinter import messagebox
 import base64
-import os
 
-# Define screen as a global variable
-screen = None
+def decrypt(message, password):
+    if password == "1234":
+        base64_bytes = base64.b64decode(message)
+        decrypted_message = base64_bytes.decode("ascii")
+        return decrypted_message
+    else:
+        return "Invalid Password"
 
-def decrypt():
-    password = code.get()
+def encrypt(message, password):
+    if password == "1234":
+        base64_bytes = base64.b64encode(message.encode("ascii"))
+        encrypted_message = base64_bytes.decode("ascii")
+        return encrypted_message
+    else:
+        return "Invalid Password"
 
-    try:
-        if password == "1234":
-            screen2 = Toplevel(screen)
-            screen2.title("decryption")
-            screen2.geometry("400x200")
-            screen2.configure(bg="#00bd56")
+def main():
+    st.title("HowsApp")
 
-            message = text1.get(1.0, END)
-            decode_message = message.encode("ascii")
-            base64_bytes = base64.b64decode(decode_message)
-            decrypt = base64_bytes.decode("ascii")
+    text_input = st.text_area("Enter text for encryption and decryption:")
+    password = st.text_input("Enter secret key for encryption and decryption:", type="password")
 
-            Label(screen2, text="DECRYPT", font="arial", fg="white", bg="#00bd56").place(x=10, y=0)
-            text2 = Text(screen2, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
-            text2.place(x=10, y=40, width=380, height=150)
+    if st.button("ENCRYPT"):
+        encrypted_message = encrypt(text_input, password)
+        st.text_area("Encrypted Text:", value=encrypted_message)
 
-            text2.insert(END, decrypt)
+    if st.button("DECRYPT"):
+        decrypted_message = decrypt(text_input, password)
+        st.text_area("Decrypted Text:", value=decrypted_message)
 
-        elif password == "":
-            messagebox.showerror("Decryption", "Input Password")
+if __name__ == "__main__":
+    main()
 
-        elif password != "1234":
-            messagebox.showerror("Decryption", "Invalid Password")
-
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred during decryption: {e}")
-
-def encrypt():
-    password = code.get()
-
-    try:
-        if password == "1234":
-            screen1 = Toplevel(screen)
-            screen1.title("encryption")
-            screen1.geometry("400x200")
-            screen1.configure(bg="#ed3833")
-
-            message = text1.get(1.0, END)
-            encode_message = message.encode("ascii")
-            base64_bytes = base64.b64encode(encode_message)
-            encrypt = base64_bytes.decode("ascii")
-
-            Label(screen1, text="ENCRYPT", font="arial", fg="white", bg="#ed3833").place(x=10, y=0)
-            text2 = Text(screen1, font="Rpbote 10", bg="white", relief=GROOVE, wrap=WORD, bd=0)
-            text2.place(x=10, y=40, width=380, height=150)
-
-            text2.insert(END, encrypt)
-
-        elif password == "":
-            messagebox.showerror("Encryption", "Input Password")
-
-        elif password != "1234":
-            messagebox.showerror("Encryption", "Invalid Password")
-
-    except Exception as e:
-        messagebox.showerror("Error", f"An error occurred during encryption: {e}")
-
-def main_screen():
-    global screen  # Define screen as a global variable
-    global code
-    global text1
-
-    try:
-        # Check if a display is available
-        if os.environ.get("DISPLAY") is None:
-            raise Exception("No display available. Tkinter requires a graphical environment.")
-
-        screen = Tk()
-        screen.geometry("375x398")
-
-        screen.title("HowsApp")
-
-        def reset():
-            code.set("")
-            text1.delete(1.0, END)
-
-        text1 = Text(font="Robote 15", bg="white", relief=GROOVE, wrap=WORD, bd=0)
-        text1.place(x=10, y=30, width=355, height=100)
-        Label(text="Enter text for encryption and decryption:", fg="black", font=("calbri", 12)).place(x=10, y=10)
-
-        Label(text="Enter secret key for encryption and decryption:", fg="black", font=("calbri", 12)).place(x=10, y=170)
-
-        code = StringVar()
-        Entry(textvariable=code, width=19, bd=0, font=("arial", 25), show="*").place(x=10, y=200)
-
-        Button(text="ENCRYPT", height="2", width=23, bg="#ed3833", fg="white", bd=0, command=encrypt).place(x=10, y=250)
-        Button(text="DECRYPT", height="2", width=23, bg="#00bd56", fg="white", bd=0, command=decrypt).place(x=200, y=250)
-        Button(text="RESET", height="2", width=50, bg="#1089ff", fg="white", bd=0, command=reset).place(x=10, y=300)
-
-        screen.mainloop()
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-main_screen()
 
